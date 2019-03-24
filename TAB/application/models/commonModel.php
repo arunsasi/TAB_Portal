dc62774c2113fbc2a84d49fa600b5c116ed88968
chins <chinschips@gmail.com>
2019-03-24 12:04:00 +0530

    common modal for join select

--- TAB/application/models/commonModel.php
+++ TAB/application/models/commonModel.php
@@ -45,6 +45,63 @@ class commonModel extends CI_Model{
 		$res = $this->db->get();
 		return $res->result();
 	}
+
+	/**
+     * @package         NTANA
+     * @author          Chinnu
+     * @since           Version 1.0
+     * Date:            23-03-2019	 
+	 * Description: 	Get data from any joined tables
+     */	
+    public function selectDataCommon($table, $columns,$condtion = NULL,$search_value = NULL,$search_like = NULL,$joins,$limit,$where_in = NULL,$where_in_data = NULL,$order_by = NULL)
+    {
+        //Getting values for DataTale
+
+        $this->db->select($columns,false)->from($table);
+        if (is_array($joins) && count($joins) > 0)
+        {
+            foreach($joins as $k => $v)
+            {
+                $this->db->join($v['table'], $v['condition'], $v['jointype']);
+            }
+        }
+        if($condtion != NULL){
+            if (is_array($condtion) && count($condtion) > 0)
+            {
+                foreach($condtion as $k => $v)
+                {  
+                     if($v['value'] != '' && $v['value'] != NULL){
+                        $this->db->where($v['key'], $v['value']);
+                     }
+                }
+            }
+        }
+        if($search_value != NULL && $search_value != ''){
+            if (is_array($search_like) && count($search_like) > 0)
+            {
+             $this->db->group_start();
+                foreach($search_like as $k )
+                {  
+                    $this->db->or_like($k,$search_value);                   
+                }
+              $this->db->group_end(); 
+            }
+        } 
+        //Where in clause
+        if($where_in != NULL && (!empty($where_in_data))){
+         $this->db->where_in($where_in,$where_in_data);
+        }
+        if(is_array($limit) && count($limit) > 0){
+            $this->db->limit($limit['length'],$limit['start']);
+        }
+        if($order_by != NULL){
+          if(is_array($order_by) && count($order_by) > 0){
+            $this->db->order_by($order_by['key'],$order_by['type']);
+          }
+        }
+        $query = $this->db->get(); 
+        return $query->result_array();
+    }
 }
 
 ?>
